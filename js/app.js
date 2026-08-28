8be453749c38b5abae37210f4aa1c0350d60f346
Nycollas <nycollas.m@escolar.ifrn.edu.br>
2026-08-28 14:27:48 -0300

    Atividade 5

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let nasc = prompt("Digite seu ano de nascimento: ");
+nasc = parseInt(nasc);
+
+let viva = confirm("Se você está viva, clique em ok.");
+
+if (viva){
+    alert(`Você tem ${2026-nasc} anos. `);
+}else{
+    alert("Você morreu.");
+} 
