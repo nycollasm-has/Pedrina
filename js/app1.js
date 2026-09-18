a4c71290cf208171efe2b4eea00f2030bfd9fb08
Nycollas <nycollas.m@escolar.ifrn.edu.br>
2026-09-18 14:29:33 -0300

    arrys

---
+++ js/app1.js
@@ -0,0 +1,7 @@
+const amigos = ["Pedro", "Ana", " Clara"];
+tam = amigos.length;
+
+console.log(`O arranjo ${amigos}`);
+console.log(`tamanho  ${tam}`);
+console.log(`primeiro ${amigos[0]}`);
+console.log(`ultimo ${amigos[tam - 1]}`);
